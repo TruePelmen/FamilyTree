5127d96ac55265bda530fd7f745fa82228907d44
TruePelmen <oleksiisenyshyn@lnu.edu.ua>
2023-10-09 20:38:47 +0300

    Database and generating random treename and inserting it in the table

--- Program.cs
+++ Program.cs
@@ -8,6 +8,41 @@ namespace FamilyTree_DB_Migration_Aattempt
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            string connectionString = "Host=localhost;Port=5432;Database=FamilyTreeAttempt;Username=postgres;Password=123321123;";
+            string testing = GeneratingRandomValuesForDB.GetRandomFamilyTreeName();
+            Console.WriteLine(testing);
+            using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open(); // Відкриваємо підключення до бази даних
+                    string insertQuery = $"INSERT INTO Дерево (\"Назва\") VALUES ('{GeneratingRandomValuesForDB.GetRandomFamilyTreeName()}');"; 
+                    using (NpgsqlCommand command = new NpgsqlCommand(insertQuery, connection))
+                    {
+                        int rowsAffected = command.ExecuteNonQuery();
+                    }
+                    string selectQuery = "SELECT * FROM Дерево";
+                    using (NpgsqlCommand command = new NpgsqlCommand(selectQuery, connection))
+                    {
+                        using (NpgsqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Console.WriteLine($"{reader.GetString(1)}");
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}");
+                }
+                finally
+                {
+                    connection.Close(); // Закриваємо підключення до бази даних
+                }
+
+            }
         }
     }
 }
