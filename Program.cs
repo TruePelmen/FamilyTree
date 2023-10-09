b3ed7eb20b1277cb43146c8ab4439219fdf677bf
TruePelmen <oleksiisenyshyn@lnu.edu.ua>
2023-10-09 19:51:54 +0300

    Npgsql

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Npgsql;
+
+namespace FamilyTree_DB_Migration_Aattempt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
